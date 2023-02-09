8ec1fbdc1d6747c8a5d04e4cb204d37441ddd7cc
Bakdaylet <123550949+Bakdaylet@users.noreply.github.com>
2023-02-09 16:38:14 +0600

    Delete 11.py

--- 11.py
+++
@@ -1,9 +0,0 @@
-def palindrome(words):
-    dict = words
-    words = words[::-1]
-    if dict == words:
-        print('Palindrome!')
-    else:
-        print("Not a palindrome")
-x = input()
-palindrome(x)
