c36939dbfd2679078af86c1bf778591d143a43be
Bakdaylet <123550949+Bakdaylet@users.noreply.github.com>
2023-02-09 16:41:31 +0600

    Delete 5.py

--- 5.py
+++
@@ -1,7 +0,0 @@
-def all_perms(somestring):
-   from itertools import permutations 
-   lst = [''.join(p) for p in permutations(somestring)]
-   for i in lst:
-       print(i)
-somestring = input()
-all_perms(somestring)
