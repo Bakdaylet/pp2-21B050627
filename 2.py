95ae08971b92bef7d8a9842e6d0e59678c671987
Bakdaylet <123550949+Bakdaylet@users.noreply.github.com>
2023-02-09 16:36:08 +0600

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,4 @@
+def Temperature(F):
+    print((5/9)*(F-32))
+F = int(input())
+Temperature(F)
---
+++ 9.py
@@ -0,0 +1,5 @@
+from  math import* 
+def Volume(radius):
+    print( (4/3)*pi*radius)
+x = float(input())
+Volume(x)
